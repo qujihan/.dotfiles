b92213b6b5a0ac2169b95ab90832a3f88a93109d
渠继涵 <qujihan@xiaohongshu.com>
2024-07-04 13:58:56 +0800

    update hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,3 +2,5 @@ hs.configdir = os.getenv('HOME') .. '/.hammerspoon'
 package.path = hs.configdir .. '/?.lua;' .. hs.configdir .. '/?/init.lua;' .. hs.configdir .. '/Spoons/?.spoon/init.lua;' .. package.path
 
 require "modules/window"
+require "modules/mouse"
+require "modules/vim"
---
+++ hammerspoon/modules/mouse.lua
@@ -0,0 +1,9 @@
+-- 中键(滚轮键)
+local changSpaceByMouse = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDragged }, function(e)
+    local mouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
+    local scrollMouseButton = 2
+    if scrollMouseButton == mouseButton then 
+        hs.alert.show("scroll")
+    end
+end)
+changSpaceByMouse:start()
