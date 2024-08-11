63b015515e4ce5785d2be0aab78354bac832f6cd
qujihan <qujihan@163.com>
2024-08-12 04:24:56 +0800

    homebrew use tsinghua mirror

--- shell/env.sh
+++ shell/env.sh
@@ -2,6 +2,15 @@ if [ -n "$DOTFILE_DEBUG" ]; then
     echo "loading env"
 fi
 #╭──────────────────────────────────────────────────────────────────────────────╮
+#│  Homebrew                                                                    │
+#╰──────────────────────────────────────────────────────────────────────────────╯
+export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
+
+#╭──────────────────────────────────────────────────────────────────────────────╮
 #│  Rust                                                                        │
 #╰──────────────────────────────────────────────────────────────────────────────╯
 export PATH=$PATH:${HOME}/.cargo/bin
