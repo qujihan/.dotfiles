a0996b3ed8299e9108c240d0615387877439338a
qujihan <qujihan@163.com>
2024-10-11 12:09:17 +0800

    update hammerspoon config

--- hammerspoon/modules/monitor_move.lua
+++ hammerspoon/modules/monitor_move.lua
@@ -2,7 +2,7 @@ local monitor_move = {}
 
 function monitor_move:init()
   hs.window.animationDuration = 0.3
-  hs.hotkey.bind({"ctrl","cmd"}, "d", function()
+  hs.hotkey.bind({"ctrl","cmd"}, "s", function()
     screen = hs.screen.mainScreen()
     hs.window.focusedWindow():moveToScreen(screen:next())
   end)
